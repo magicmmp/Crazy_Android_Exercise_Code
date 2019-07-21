79085d125e401b4a4507e1a8785e9d68c109b56c
magicmmp <2233973404@qq.com>
2019-07-21 16:47:26 +0800

    第一次提交到码云

---
+++ app/src/main/java/com/example/pss/crazy_android_exercise_code/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.pss.crazy_android_exercise_code;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
